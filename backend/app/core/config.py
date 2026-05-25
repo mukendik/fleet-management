aa6e2ef3a80b72a09b7e9a73a25a43aac2efe0a9
gmukendi <gmukendi@gmail.com>
2026-05-25 21:44:10 +0200

    config file upraded

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,16 +1,13 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-
     DATABASE_URL: str
 
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str = "dev_secret"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
