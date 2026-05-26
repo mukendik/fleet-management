c15f86878b39f2f16ffcb9c8cc9a22ffdb780b8c
gmukendi <gmukendi@gmail.com>
2026-05-26 20:23:24 +0200

    feat: add company model

---
+++ backend/app/models/company.py
@@ -0,0 +1,9 @@
+
+from sqlalchemy import Column, Integer, String
+from app.models.base import Base
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True)
