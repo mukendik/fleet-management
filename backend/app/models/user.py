2105bbda0bb0984ee46f4b0bc44185100e5cba5b
gmukendi <gmukendi@gmail.com>
2026-05-26 20:25:03 +0200

    feat: add user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from app.models.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    company_id = Column(Integer, ForeignKey("companies.id"))
