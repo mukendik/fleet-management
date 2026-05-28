3afc24f2ce48e9a51b34cb22ea2b3224232dab2c
gmukendi <gmukendi@gmail.com>
2026-05-28 17:02:24 +0200

    feat: authenticate user service

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,9 +1,8 @@
 from sqlalchemy.orm import Session
-
 from app.models.user import User
 from app.schemas.auth import UserRegister
 from app.core.security import hash_password
-
+from app.core.security import verify_password
 
 def create_user(db: Session, user_data: UserRegister):
 
@@ -18,3 +17,18 @@ def create_user(db: Session, user_data: UserRegister):
     db.refresh(user)
 
     return user
+
+def authenticate_user(
+            db: Session,
+            email: str,
+            password: str
+):
+            user = db.query(User).filter(User.email == email).first()
+
+            if not user:
+                return None
+
+            if not verify_password(password, user.hashed_password):
+                return None
+
+            return user
