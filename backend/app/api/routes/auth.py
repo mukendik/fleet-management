78a2f3fbd25c38bd54c959c197e255fc5a8210c0
gmukendi <gmukendi@gmail.com>
2026-05-28 17:23:17 +0200

    feat: login auth route

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -5,8 +5,12 @@ from app.db.session import get_db
 from app.models.user import User
 
 from app.schemas.auth import UserRegister, UserLogin, Token
+from app.schemas.user import UserLogin
 
 from app.services.auth_service import create_user
+from app.services.auth_service import authenticate_user
+
+from app.core.security import create_access_token
 
 from app.core.security import (
     verify_password,
@@ -22,21 +26,22 @@ def register(user_data: UserRegister, db: Session = Depends(get_db)):
 
     return {"message": "user created"}
 
-
 @router.post("/login", response_model=Token)
 def login(user_data: UserLogin, db: Session = Depends(get_db)):
 
-    user = db.query(User).filter(User.email == user_data.email).first()
+        user = authenticate_user(
+             db,
+             user_data.email,
+             user_data.password
+        )
 
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        if not user:
+           raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    if not verify_password(user_data.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        
+       token = create_access_token({"sub": user.email})
 
-    token = create_access_token({"sub": user.email})
-
-    return {
-        "access_token": token,
-        "token_type": "bearer",
-    }
+       return {
+           "access_token": token,
+           "token_type": "bearer",
+       }
