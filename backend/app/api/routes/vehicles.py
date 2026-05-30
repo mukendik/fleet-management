ae601bbe7e77e6ae681dc74d6a8dd385f9b61c17
gmukendi <gmukendi@gmail.com>
2026-05-30 17:11:29 +0200

    CRUD vehicle API

--- backend/app/api/routes/vehicles.py
+++ backend/app/api/routes/vehicles.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.api.deps import get_db, get_current_user
 from app.models.user import User
 from app.models.vehicle import Vehicle
+from app.schemas.vehicle import VehicleCreate, VehicleUpdate
+
+
 
 router = APIRouter(prefix="/vehicles", tags=["Vehicles"])
 
@@ -16,3 +19,70 @@ def get_vehicles(
     return db.query(Vehicle).filter(
         Vehicle.company_id == current_user.company_id
     ).all()
+
+
+#CREATE VEHICLE
+@router.post("")
+def create_vehicle(
+    data: VehicleCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    vehicle = Vehicle(
+        name=data.name,
+        plate_number=data.plate_number,
+        company_id=current_user.company_id
+    )
+
+    db.add(vehicle)
+    db.commit()
+    db.refresh(vehicle)
+
+    return vehicle
+
+# UPDATE VEHICLE
+@router.put("/{vehicle_id}")
+def update_vehicle(
+    vehicle_id: int,
+    data: VehicleUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    vehicle = db.query(Vehicle).filter(
+        Vehicle.id == vehicle_id,
+        Vehicle.company_id == current_user.company_id
+    ).first()
+
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    if data.name is not None:
+        vehicle.name = data.name
+
+    if data.plate_number is not None:
+        vehicle.plate_number = data.plate_number
+
+    db.commit()
+    db.refresh(vehicle)
+
+    return vehicle
+
+#DELETE VEHICLE
+@router.delete("/{vehicle_id}")
+def delete_vehicle(
+    vehicle_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    vehicle = db.query(Vehicle).filter(
+        Vehicle.id == vehicle_id,
+        Vehicle.company_id == current_user.company_id
+    ).first()
+
+    if not vehicle:
+        raise HTTPException(status_code=404, detail="Vehicle not found")
+
+    db.delete(vehicle)
+    db.commit()
+
+    return {"message": "Vehicle deleted successfully"}
